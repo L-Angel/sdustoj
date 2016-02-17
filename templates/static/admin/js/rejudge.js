453820e8c01ba25b0339951ea7d05d324e110312
Rick <jiangchuanwei2012@163.com>
2016-02-18 05:43:32 +0800

    添加验证码功能； mysql连接数增加； 由于数据库和web分布在不同服务器，网络速度成为查询瓶颈，导致ranklist打开过慢； web迁移，由Django服务器迁移至nginx＋uwsig; 修复从problem提交代码报错； 修复分页bug； 修复判题模块更新编译错误信息文本错误； 待修复问题：验证码功能，数据库查询速度过快，缓存过小问题，连接数过大问题，ranklist页面查询优化；

--- templates/static/admin/js/rejudge.js
+++ templates/static/admin/js/rejudge.js
@@ -6,10 +6,10 @@ $(function(){
 
 });
  function alert_success(){
-         var txt=  "题目文件添加成功！";
+         var txt=  "Rejudge成功！";
                     window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.success);
        }
         function alert_warning(){
-         var txt=  "题目文件添加失败！";
+         var txt=  "Rejudge失败！";
                     window.wxc.xcConfirm(txt, window.wxc.xcConfirm.typeEnum.warning);
        }
